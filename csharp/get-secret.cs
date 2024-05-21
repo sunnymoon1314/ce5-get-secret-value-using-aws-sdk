0d7883d46f5e62c077473351d61f1d722b4831fe
sunnymoon1314 <moonysun1314@gmail.com>
2024-05-21 19:33:54 +0800

    Updated get-secret.yml to include dotnet build.

--- csharp/get-secret.cs
+++ csharp/get-secret.cs
@@ -11,42 +11,39 @@ using Amazon;
 using Amazon.SecretsManager;
 using Amazon.SecretsManager.Model;
 
-namespace Main
+public class GetSecretValueAsync
 {
-    class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+        string secretName = "soon-secret-name";
+        string region = "us-east-1";
+
+        IAmazonSecretsManager client = new AmazonSecretsManagerClient(RegionEndpoint.GetBySystemName(region));
+
+        GetSecretValueRequest request = new GetSecretValueRequest
         {
-            string secretName = "soon-secret-name";
-            string region = "us-east-1";
+            SecretId = secretName,
+            VersionStage = "AWSCURRENT", // VersionStage defaults to AWSCURRENT if unspecified.
+        };
 
-            IAmazonSecretsManager client = new AmazonSecretsManagerClient(RegionEndpoint.GetBySystemName(region));
+        GetSecretValueResponse response;
 
-            GetSecretValueRequest request = new GetSecretValueRequest
-            {
-                SecretId = secretName,
-                VersionStage = "AWSCURRENT", // VersionStage defaults to AWSCURRENT if unspecified.
-            };
-
-            GetSecretValueResponse response;
-
-            try
-            {
-                response = await client.GetSecretValueAsync(request);
-            }
-            catch (Exception e)
-            {
-                // For a list of the exceptions thrown, see
-                // https://docs.aws.amazon.com/secretsmanager/latest/apireference/API_GetSecretValue.html
-                throw e;
-            }
-
-            string secret = response.SecretString;
-
-            // Your code goes here
-
-            // Expected output: {"soon-secret-key":"soon-secret-value"}
-            Console.WriteLine("Secret: ", secret);
+        try
+        {
+            response = await client.GetSecretValueAsync(request);
         }
+        catch (Exception e)
+        {
+            // For a list of the exceptions thrown, see
+            // https://docs.aws.amazon.com/secretsmanager/latest/apireference/API_GetSecretValue.html
+            throw e;
+        }
+
+        string secret = response.SecretString;
+
+        // Your code goes here
+
+        // Expected output: {"soon-secret-key":"soon-secret-value"}
+        Console.WriteLine("Secret: ", secret);
     }
 }
