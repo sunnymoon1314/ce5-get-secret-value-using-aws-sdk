5a7a1e88a52dcd768ca12c3dba4f480c2e4321f7
sunnymoon1314 <moonysun1314@gmail.com>
2024-05-21 18:16:05 +0800

    Updated get-secret.yml to include csharp-get-secret job.

--- go/get-secret.go
+++ go/get-secret.go
@@ -2,6 +2,13 @@
 // If you need more information about configurations or implementing the sample code, visit the AWS docs:
 // https://aws.github.io/aws-sdk-go-v2/docs/getting-started/
 
+// 21.05.2024 Soon: The new build tag syntax was introduced in Go 1.17 and later version.
+// https://github.com/bitfield/script/issues/131
+// Hence, please do not use go version <= 1.16. Otherwise, you will encounter the following errors:
+// Error: ../../../go/pkg/mod/github.com/aws/aws-sdk-go-v2/credentials@v1.17.15/ssocreds/sso_credentials_provider.go:9:2: //go:build comment without // +build comment
+// Error: ../../../go/pkg/mod/github.com/aws/aws-sdk-go-v2/credentials@v1.17.15/ssocreds/sso_token_provider.go:11:2: //go:build comment without // +build comment
+// Error: ../../../go/pkg/mod/github.com/aws/aws-sdk-go-v2/credentials@v1.17.15/stscreds/assume_role_provider.go:108:2: //go:build comment without // +build comment
+
 package main
 
 import (
